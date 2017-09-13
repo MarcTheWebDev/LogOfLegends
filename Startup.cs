0a799872f8045d8f095a9b90ea006200137551bd
MarcTheWebDev <marcusdsaunders@gmail.com>
2017-09-12 22:58:28 -0400

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LogOfLegends.Startup))]
+namespace LogOfLegends
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
